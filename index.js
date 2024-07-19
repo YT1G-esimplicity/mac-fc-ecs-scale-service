873fdabaac21574521a82a7e821d0fd114cb07cf
YT1G-esimplicity <aliyu.yeh@esimplicity.com>
2024-07-19 11:51:56 -0400

    GHA ECS service scale action

--- index.js
+++ index.js
@@ -14,11 +14,30 @@ ecs.config.getCredentials((error) => {
 try {
   const cluster = core.getInput("cluster", { required: true });
   const service = core.getInput("service", { required: true });
-  const desiredCount = core.getInput("desired-count", { required: true });
-  core.info(
-    `Setting desired task count to ${desiredCount} for service ${service} in cluster ${cluster}`
-  );
-  await ecs.updateService({ service, cluster, desiredCount }).promise();
+  const desiredCount = core.getInput("desired-count");
+  const adjustCount = core.getInput("adjust-count");
+
+  if (desiredCount) {
+    core.info(
+      `Setting desired task count to ${desiredCount} for service ${service} in cluster ${cluster}`
+    );
+    await ecs.updateService({ service, cluster, desiredCount }).promise();
+  } else if (adjustCount) {
+    const describeServicesResponse = await ecs.describeServices({
+      services: [service],
+      cluster,
+    }).promise();
+
+    const currentDesiredCount = describeServicesResponse.services[0].desiredCount;
+    const newDesiredCount = currentDesiredCount + parseInt(adjustCount, 10);
+
+    core.info(
+      `Adjusting task count by ${adjustCount}. New desired task count is ${newDesiredCount} for service ${service} in cluster ${cluster}`
+    );
+    await ecs.updateService({ service, cluster, desiredCount: newDesiredCount }).promise();
+  } else {
+    core.setFailed("Either 'desired-count' or 'adjust-count' must be provided.");
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
